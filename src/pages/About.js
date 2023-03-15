cac9becc8e36407d03bc7e777ba54f24a5e1d54d
Albert <ishengomaalbert@gmail.com>
2023-03-15 14:51:20 +0300

    Legacy react router

---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+function About(props) {
+  return (
+    <div className="my-10">
+      <h1 className="text-2xl font-semibold my-8">About</h1>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed ipsa
+        porro sit qui hic repudiandae nulla eveniet at! Molestiae laborum ab
+        repellendus ad aspernatur nemo quae vitae aliquam debitis dolore
+        numquam, hic ea velit natus cumque, voluptatibus quisquam cum voluptatem
+        possimus explicabo? Sed optio neque quis id ad! Minus soluta voluptatum
+        veniam at doloremque. Quibusdam, voluptate ducimus autem odit libero
+        modi ratione nesciunt alias ipsum accusantium eveniet esse magnam harum
+        molestias nobis mollitia commodi atque deleniti, rem iusto minima illum.
+        Exercitationem, qui blanditiis hic velit quidem quos nostrum, delectus
+        officiis, fuga minima ducimus? Error amet sequi dignissimos, omnis
+        nesciunt nam adipisci exercitationem totam voluptate sit tempora
+        explicabo. Voluptatem reiciendis soluta est pariatur assumenda eos
+        officia ea accusamus velit. Provident quis ratione, cupiditate,
+        quibusdam, distinctio suscipit obcaecati reprehenderit necessitatibus
+        libero tempore dicta voluptatem? Sapiente architecto nostrum
+        consequuntur ullam, reiciendis iure molestiae saepe voluptas delectus
+        repellat natus? Consequatur voluptas placeat dolore.
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+function Home(props) {
+  return (
+    <div className="my-10">
+      <h1 className="text-2xl font-semibold my-8">Welcome</h1>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        distinctio saepe, labore ut aperiam sunt! Voluptates nobis, tempora
+        necessitatibus blanditiis minima nostrum reiciendis vel ad temporibus,
+        aliquam architecto. Possimus necessitatibus blanditiis hic numquam.
+        Exercitationem quae amet nulla error quas, quia libero qui impedit
+        debitis voluptas quam atque temporibus quasi? Quibusdam temporibus saepe
+        autem sit, est facilis doloremque pariatur itaque dolores, sequi
+        consequuntur quia, odio laboriosam? Officiis dolores, laboriosam
+        obcaecati rerum tenetur porro facere sint ut, asperiores nobis
+        architecto non maiores consectetur aut sequi incidunt nostrum molestiae
+        adipisci voluptatibus harum id nam. Nam ea adipisci autem! Reprehenderit
+        eligendi labore earum excepturi.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
