86ad2860e083250facc9e59465eeb8aa632f657d
Albert <ishengomaalbert@gmail.com>
2023-03-20 14:39:34 +0300

    UI modification & code cleanup

--- src/layouts/HelpLayout.js
+++ src/layouts/HelpLayout.js
@@ -1,4 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { ChevronRight } from "../components/Icons";
 
 function HelpLayout(props) {
   return (
@@ -8,9 +9,13 @@ function HelpLayout(props) {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, cum!
       </p>
 
-      <nav class="flex items-center justify-center my-8" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-1 md:space-x-3">
-          <li class="inline-flex items-center">
+      {/* BreadCrumbs */}
+      <nav
+        className="flex items-center justify-center my-8"
+        aria-label="Breadcrumb"
+      >
+        <ol className="inline-flex items-center space-x-1 md:space-x-3">
+          <li className="inline-flex items-center">
             <NavLink
               to="faq"
               className={({ isActive }) =>
@@ -23,20 +28,8 @@ function HelpLayout(props) {
             </NavLink>
           </li>
           <li>
-            <div class="flex items-center">
-              <svg
-                aria-hidden="true"
-                class="w-6 h-6 text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+            <div className="flex items-center">
+              <ChevronRight />
               <NavLink
                 to="contact"
                 className={({ isActive }) =>
@@ -51,11 +44,8 @@ function HelpLayout(props) {
           </li>
         </ol>
       </nav>
-      {/* <nav className="flex items-center space-x-4 my-7">
-        <NavLink className={({ isActive }) => (isActive ? "underline font-semibold" : "")} to="faq">View the FAQ</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "underline font-semibold" : "")} to="contact">Contact Us</NavLink>
-      </nav> */}
 
+      {/* Page Output */}
       <Outlet />
     </div>
   );
