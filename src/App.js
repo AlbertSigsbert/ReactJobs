f48a56fd59c2d098e7c76d707c5a5cf528778299
Albert <ishengomaalbert@gmail.com>
2023-03-20 12:55:09 +0300

    UI fixs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 
 //components
-import Navbar from "./components/Navbar";
+
 
 //pages
 import About from "./pages/About";
@@ -10,7 +10,13 @@ import Home from "./pages/Home";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <header className="mx-[6%] py-4">
+        <nav className="flex gap-4 justify-end">
+          <Link className="mr-auto text-2xl font-bold" to="/">ReactJobs</Link>
+          <NavLink className={({isActive}) => isActive ? 'underline': ''} to="/">Home</NavLink>
+          <NavLink className={({isActive}) => isActive ? 'underline': ''} to="about">About</NavLink>
+        </nav>
+      </header>
       <main className="mx-[6%]">
         <Routes>
           <Route index element={<Home />} />
